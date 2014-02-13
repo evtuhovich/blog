87fc71c559874d2a94c9faff7d28c92a533732cb
Ivan Evtukhovich <evtuhovich@gmail.com>
2014-02-13 21:28:14 +0400

    First working version

--- config.rb
+++ config.rb
@@ -68,22 +68,28 @@ activate :blog do |blog|
   blog.prefix = "blog"
   blog.permalink = ":year/:month/:day/:title/"
   # blog.sources = ":year-:month-:day-:title.html"
-  # blog.taglink = "tags/:tag.html"
-  blog.layout = "blog_layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
+  blog.taglink = "categories/:tag/"
+  blog.layout = false
+  blog.summary_separator = /(<!-- more -->)/
+  blog.summary_length = nil
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
   # blog.default_extension = ".markdown"
 
   blog.tag_template = "blog/tag.html"
-  blog.calendar_template = "blog/calendar.html"
+#  blog.calendar_template = "blog/calendar.html"
 
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/:num"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "/blog/page/:num"
   # blog.publish_future_dated = true
+  # blog.custom_collections = {
+  #   categories: {
+  #     link: '/categories/{categories}/',
+  #     template: '/category.html'
+  #   }
+  # }
 end
 # Build-specific configuration
 configure :build do
